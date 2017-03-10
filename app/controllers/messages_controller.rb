f89da069207e61b816c32481eab3b6a3518b3716
yoko978 <0909978@gmail.com>
2017-03-10 13:57:59 +0000

    challenge1 is completed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,20 @@
 class MessagesController < ApplicationController
+  
   def index
-    @message =  Message.new
     @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    @message =  Message.new(message_params)
+    if @message.save
+      redirect_to root_path , notice:'メッサージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @message = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
   end
   
   def create
@@ -16,9 +29,10 @@ class MessagesController < ApplicationController
     end
   end
 
-  private
+ private
 
   def message_params
     params.require(:message).permit(:name, :body)
   end
 end
+
